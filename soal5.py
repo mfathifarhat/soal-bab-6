ba51a6667c1d22c2170c95212f200532ee948d61
Muhammad Fathi Farhat <mfathifarhat72@gmail.com>
2025-10-19 21:51:41 +0700

    nomer 5 done bang

--- soal5.py
+++ soal5.py
@@ -2,6 +2,32 @@ N = int(input().strip())
 
 strings = [input().strip() for _ in range(N)]
 
+matrix = [[i for i in range(len(strings))]]
+
+terpanjang = []
+
 for i in strings:
-    pass
+    panjang = 0
+    frekuensi = {}
+    isGanjil = False
     
+    for s in i:
+        if s in frekuensi:
+            frekuensi[s] += 1
+        else:
+            frekuensi[s] = 1
+    
+    for a in frekuensi.values():
+        if a % 2 == 0:
+            panjang += a
+        else:
+            panjang += (a - 1)
+            isGanjil = True
+            
+    if isGanjil == True:
+        panjang += 1
+            
+    terpanjang.append(panjang)
+    
+for i in terpanjang:
+    print(i)
